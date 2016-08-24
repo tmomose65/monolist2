dff6238d39a0973feea6c7473415a27e822e40af
tmomose <t.momose65@gmail.com>
2016-08-24 00:15:06 +0000

    test

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -12,6 +12,11 @@ class OwnershipsController < ApplicationController
     if @item.new_record?
       # TODO 商品情報の取得 RakutenWebService::Ichiba::Item.search を用いてください
       items = {}
+      response = RakutenWebService::Ichiba::Item.search(
+        keyword: params[:item_Code],
+        imageFlag: 1,
+      )
+      items = response
 
       item                  = items.first
       @item.title           = item['itemName']
@@ -25,7 +30,11 @@ class OwnershipsController < ApplicationController
     # TODO ユーザにwant or haveを設定する
     # params[:type]の値にHaveボタンが押された時には「Have」,
     # Wantボタンが押された時には「Want」が設定されています。
-    
+    if params[:type] == "Have"
+      current_user.have(@item)
+    else
+      current_user.want(@item)
+    end
 
   end
 
@@ -35,6 +44,10 @@ class OwnershipsController < ApplicationController
     # TODO 紐付けの解除。 
     # params[:type]の値にHave itボタンが押された時には「Have」,
     # Want itボタンが押された時には「Want」が設定されています。
-
+    if params[:type] == "Have"
+      current_user.unhave(@item)
+    else
+      current_user.unwant(@item)
+    end
   end
 end
